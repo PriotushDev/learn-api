459919a7625c0ae93bbde929033f871b39398c52
Priotush <ai.priyobabu@gmail.com>
2026-01-29 15:34:43 +0600

    project install

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json([
+            'message' => "Hello Student",
+            'status' => 'success',
+            'data' =>$students,
+        ]);
+    }
+
+    public function store(Reqeust $request)
+    {
+        $student = Student::create([
+            'name' => $request->name,
+            'email'=> $request->email,
+            'mobile' => $request->mobile,
+        ]);
+
+        return response()->json([
+            'message' => "Hello Student",
+            'status' => 'success',
+            'data' =>$students,
+        ]);
+    }
+}
